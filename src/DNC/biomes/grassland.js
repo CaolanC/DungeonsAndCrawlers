eaf4dc7430ba600158400134bacd5dca0614810e
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-01-15 18:24:43 +0000

    removed redundant comment

--- src/DNC/biomes/grassland.js
+++ src/DNC/biomes/grassland.js
@@ -3,4 +3,3 @@ export default {
 	moisture: 0.5,
 	temperature: 0.5,
 };
-// what does temperature and moisture imply for this biome? I was wondering whether I should specify amplitude for the biomes height, but is this it?
