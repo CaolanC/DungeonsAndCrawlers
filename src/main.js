68ce7a02c11e89ac6678cb95bbcac5d4360e1d79
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-01-20 13:16:27 +0000

    added first get endpoint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5173;
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -9,6 +14,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
+app.get('/join', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'game.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
+
