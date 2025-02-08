2ea87d97501a566337cbc3d07c627b2286f1c908
doylee69 <doylee69@mail.dcu.ie>
2025-02-08 18:46:12 +0000

    Physics and basic movement

--- public/js/init.js
+++ public/js/init.js
@@ -4,11 +4,15 @@ import * as CANNON from "https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm";
 
 console.log("Working in public");
 
+const JUMP_FORCE = 5; // Velocity applied on jump
+const SPEED = 17; // Velocity applied on WASD movement
+
 export class Player
 {
     constructor(scene, world){ // scene and world of threejs render
         this.scene = scene;
         this.world = world;
+        this.onGround = false;
 
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
@@ -68,37 +72,20 @@ scene.add(sun);
 const ambientLight = new THREE.AmbientLight(0x404040, 0.5);
 scene.add(ambientLight);
 
-const move = { forward: 0, backward: 0, left: 0, right: 0, up: 0, down: 0 };
-const speed = 0.2;
-let rotation = { x: 0, y: 0 };
-let sensitivity = 0.002;
-
 // Cannon-es.js
-
-// const testGeometry = new THREE.BoxGeometry(1, 1, 1);
-// const testMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-// const testCube = new THREE.Mesh(testGeometry, testMaterial);
-// testCube.position.set(0, 10, 0);
-// scene.add(testCube);
 
 const testCubeGeom = new THREE.BoxGeometry(1, 1, 1);
 const testCubeMat = new THREE.MeshStandardMaterial({ color: 0xffffff });
 const testCube = new THREE.Mesh(testCubeGeom, testCubeMat);
-testCube.position.set(0, 10, 0);
+testCube.position.set(0, 3, 0);
 scene.add(testCube);
 
-// const wallGeom = new THREE.BoxGeometry(5, 5, 1);
-// const wallMat = new THREE.MeshStandardMaterial({ color: 0xffffff });
-// const wall = new THREE.Mesh(wallGeom, wallMat);
-// wall.position.set(0, 2.5, -2);
-// scene.add(wall);
-
 const world = new CANNON.World({
-    gravity: new CANNON.Vec3(0, -9.81, 0),
+    gravity: new CANNON.Vec3(0, -9.5, 0),
 });
 
-world.broadphase = new CANNON.SAPBroadphase(world); // More efficient than NaïveBroadphase
-world.allowSleep = true; // Allows sleeping to prevent unnecessary calculations
+world.broadphase = new CANNON.SAPBroadphase(world); 
+world.allowSleep = true; 
 
 const radius = 0.5;
 const halfExtents = new CANNON.Vec3(radius, radius, radius);
@@ -107,8 +94,11 @@ const cubeBody = new CANNON.Body({
     mass: 5,
     shape: new CANNON.Box(halfExtents),
 })
-cubeBody.position.set(0, 10, 0);
+cubeBody.position.set(0, 3, 0);
 world.addBody(cubeBody);
+
+cubeBody.fixedRotation = true;
+cubeBody.updateMassProperties();
 
 const cubeBody2 = new CANNON.Body({
     mass: 0,
@@ -125,27 +115,44 @@ const groundBody = new CANNON.Body({
 groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 world.addBody(groundBody);
 
+const playerMaterial = new CANNON.Material("playerMaterial");
+const voxelMaterial = new CANNON.Material("voxelMaterial");
+
+const contactMaterial = new CANNON.ContactMaterial(playerMaterial, voxelMaterial , {
+    friction: 0.55,
+    restitution: 0.0,
+    contactEquationStiffness: 1e8,
+    contactEquationRelaxation: 4,
+});
+world.addContactMaterial(contactMaterial);
+
+cubeBody.material = playerMaterial;
+cubeBody2.material = voxelMaterial;
+
 document.body.addEventListener('click', () => document.body.requestPointerLock());
+
+const move = { forward: false, backward: false, left: false, right: false, jump: false };
+const speed = 0.2;
+let rotation = { x: 0, y: 0 };
+let sensitivity = 0.002;
 
 document.addEventListener('keydown', (e) => {
     switch (e.key) {
-        case 'w': move.forward = 1; break;
-        case 's': move.backward = 1; break;
-        case 'a': move.left = 1; break;
-        case 'd': move.right = 1; break;
-        case ' ': move.up = 1; break;
-        case 'Shift': move.down = 1; break;
+        case 'w': move.forward = true; break;
+        case 's': move.backward = true; break;
+        case 'a': move.left = true; break;
+        case 'd': move.right = true; break;
+        case ' ': move.jump = true; break;
     }
 });
 
 document.addEventListener('keyup', (e) => {
     switch (e.key) {
-        case 'w': move.forward = 0; break;
-        case 's': move.backward = 0; break;
-        case 'a': move.left = 0; break;
-        case 'd': move.right = 0; break;
-        case ' ': move.up = 0; break;
-        case 'Shift': move.down = 0; break;
+        case 'w': move.forward = false; break;
+        case 's': move.backward = false; break;
+        case 'a': move.left = false; break;
+        case 'd': move.right = false; break;
+        case ' ': move.jump = false; break;
     }
 });
 
@@ -157,79 +164,74 @@ document.addEventListener('mousemove', (e) => {
     }
 });
 
-function updateCamera() {
-    const direction = new THREE.Vector3();
-    const right = new THREE.Vector3();
-    const up = new THREE.Vector3(0, 1, 0);
+// function updateCamera() {
+//     const direction = new THREE.Vector3();
+//     const right = new THREE.Vector3();
+//     const up = new THREE.Vector3(0, 1, 0);
 
-    camera.getWorldDirection(direction);
-    direction.cross(up).normalize();
+//     camera.getWorldDirection(direction);
+//     direction.cross(up).normalize();
 
-    const moveVector = new THREE.Vector3();
-    if (move.forward) moveVector.add(camera.getWorldDirection(new THREE.Vector3()).normalize());
-    if (move.backward) moveVector.add(camera.getWorldDirection(new THREE.Vector3()).normalize().negate());
-    if (move.left) moveVector.add(direction.negate());
-    if (move.right) moveVector.add(direction.negate().negate());
-    if (move.up) moveVector.y += 1;
-    if (move.down) moveVector.y -= 1;
+//     const moveVector = new THREE.Vector3();
+//     if (move.forward) moveVector.add(camera.getWorldDirection(new THREE.Vector3()).normalize());
+//     if (move.backward) moveVector.add(camera.getWorldDirection(new THREE.Vector3()).normalize().negate());
+//     if (move.left) moveVector.add(direction.negate());
+//     if (move.right) moveVector.add(direction.negate().negate());
+//     if (move.up) moveVector.y += 1;
+//     if (move.down) moveVector.y -= 1;
 
-    moveVector.normalize().multiplyScalar(speed);
-    camera.position.add(moveVector);
+//     moveVector.normalize().multiplyScalar(speed);
+//     camera.position.add(moveVector);
 
-    camera.rotation.x = rotation.x;
-    camera.rotation.y = rotation.y;
-}
+//     camera.rotation.x = rotation.x;
+//     camera.rotation.y = rotation.y;
+// }
 
+const player = new Player(scene, world);
+player.playerbody.material = playerMaterial;
+groundBody.material = voxelMaterial;
+player.playerbody.fixedRotation = true;
+player.playerbody.updateMassProperties();
 
-// TODO: Update the physics to any new potential errors in future. May have to code in gravity manually.
+player.playerbody.addEventListener("collide", (e) => {
+    if(e.body == groundBody) {
+        console.log("player is on ground");
+        player.onGround = true;
+    }
+});
 
-// cubeBody.addEventListener('collide', (event) => {
-//     console.log("Collision detected with:", event.body);
-//     cubeBody.velocity.set(0, 0, 0);
-//     cubeBody.angularVelocity.set(0, 0, 0);
-//     cubeBody.sleep();
-//     // Wake up the sphere after 1 second
-//     setTimeout(() => {
-//         cubeBody.wakeUp();
-//         console.log("Sphere body woke up after 1 second");
-//     }, 1000); // 1000 milliseconds = 1 second
-// });
+function updatePlayer(player) {
+    const currentY = player.playerbody.velocity.y;
 
-// Fix to to-do above. Needs more testing.
-// TODO: Add a position checker to make sure object is not inside another object to prevent slight bouncing.
-
-
-
-function checkcollide() {
-    let isColliding = false;
-
-    world.contacts.forEach((contact) => {
-        if (contact.bi === cubeBody || contact.bj === cubeBody) {
-            isColliding = true;
-        }
-    });
-
-    console.log(isColliding);
-
-    if (isColliding) {
-        // cubeBody.position.set(cubeBody.position.x, Math.ceil(cubeBody.position.y), cubeBody.position.z);
-        cubeBody.velocity.set(0, 0, 0);
-        cubeBody.angularVelocity.set(0, 0, 0);
-        cubeBody.sleep();
+    if (move.forward) {
+        player.playerbody.velocity.z = -SPEED;
+    } else if (move.backward) {
+        player.playerbody.velocity.z = SPEED;
     } else {
-        if (cubeBody.sleepState === CANNON.Body.SLEEPING) {
-            cubeBody.wakeUp();
-        }
+        player.playerbody.velocity.z = 0;
+    }
+    
+    if (move.right) {
+        player.playerbody.velocity.x = SPEED;
+    } else if (move.left) {
+        player.playerbody.velocity.x = -SPEED;
+    } else {
+        player.playerbody.velocity.x = 0;
+    }
+
+    if (move.jump && player.onGround) {
+        player.playerbody.velocity.y = JUMP_FORCE;
+        player.onGround = false;  
+    } else {
+        player.playerbody.velocity.y = currentY;
     }
 }
 
-const player = new Player(scene, world);
-
 function animate() {
     requestAnimationFrame(animate);
-    updateCamera();
+    // updateCamera();
+    updatePlayer(player);
     world.fixedStep();
-    checkcollide();
     player.playercube.position.copy(player.playerbody.position);
     testCube.position.copy(cubeBody.position);
     cube.position.copy(cubeBody2.position);
