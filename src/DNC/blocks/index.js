a7531e715b27b6e1f03c5efa37e0caa05673983e
Caolan Cochrane <caolancochrane@gmail.com>
2025-02-18 23:34:44 +0000

    typo

--- src/DNC/blocks/index.js
+++ src/DNC/blocks/index.js
@@ -1,9 +1,9 @@
 import Air from "./air.js";
 import Stone from "./stone.js";
-import Grass from "./stone.js";
-import Dirt from "./stone.js";
+import Grass from "./grass.js";
+import Dirt from "./dirt.js";
 import Sand from "./stone.js";
-import Chocolate from "./stone.js";
+import Chocolate from "./chocolate.js";
 import Snow from "./stone.js";
-import Ice from "./stone.js";
+import Ice from "./ice.js";
 export const DNC_Blocks = [Air, Stone, Grass, Dirt, Sand, Chocolate, Snow, Ice];
