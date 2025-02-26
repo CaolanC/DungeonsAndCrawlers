f0973bde89ef9af35d788f7d4af6d70a1adf7605
Caolan Cochrane <caolan.cochrane2@mail.dcu.ie>
2025-02-26 08:25:33 +0000

    prod updates

--- public/js/NetworkManager.js
+++ public/js/NetworkManager.js
@@ -1,20 +1,35 @@
 export class NetworkManager {
     constructor(url) {
-        this.ws = new WebSocket(url);
+        this.url = url;
         this.handlers = {};
+        this.connect();
+    }
+
+    connect() {
+        this.ws = new WebSocket(this.url);
         this.readyPromise = new Promise((resolve) => {
             this.ws.onopen = () => {
                 console.log("Connected to WebSocket");
-                resolve();
             };
         });
-
         this.ws.onmessage = this.handleMessage.bind(this);
-    }
+        this.ws.onerror = (err) => {
+            console.error("WebSocket Error:", err);
+        };
+
+        this.ws.onclose = () => {
+            console.warn("WebSocket closed. Reconnecting in 3 seconds...");
+            setTimeout(() => this.connect(), 3000);
+        };
+    };
 
     async send(type, payload) {
         await this.readyPromise; // Wait until WebSocket is open
-        this.ws.send(JSON.stringify({ type, ...payload }));
+        if (this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(JSON.stringify({ type, ...payload }));
+        } else {
+            console.error("WebSocket is not open. Failed to send message:", type, payload);
+        }
     }
 
     on(eventType, callback) {
@@ -28,11 +43,3 @@ export class NetworkManager {
         }
     }
 }
-
-function test() {
-    const nm = new NetworkManager("ws://localhost:5173");
-    nm.send("player_update", {
-        position: "x,x,z,"
-    });
-};
-//test();
